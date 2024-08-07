dba5d90c059bc22671ade296da9948669b578be9
Filipe Fernandes <ocefpaf@gmail.com>
2024-08-07 09:53:05 +0200

    use file scripts to avoid rerendering mangling the recipe

---
+++ recipe/bld.bat
@@ -0,0 +1,1 @@
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv --global-option="--with-libyaml" --global-option="build_ext" --global-option="-I%LIBRARY_INC%" --global-option="-L%LIBRARY_LIB%"
